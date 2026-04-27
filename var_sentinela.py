71298dfaa9e77ed7c2ab8c091f5c3c8306de0085
HenricoDeSistemas <henricoiswilliam@gmail.com>
2026-04-27 16:07:52 +0000

    versão 1

--- var_sentinela.py
+++ var_sentinela.py
@@ -1,1 +1,8 @@
+#Exemplo de Uso da variavel sentinela
 
+while True:
+    comando = input("Digite um comando-para parar digite 'sair'")
+
+if comando == "sair":
+    break
+print(F"Executando:{comando}")
