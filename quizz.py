91c7589c51b4968882eaf509ef60050c92ccee1a
HenricoDeSistemas <henricoiswilliam@gmail.com>
2026-05-04 16:03:25 +0000

    v2

--- quizz.py
+++ quizz.py
@@ -1,5 +1,10 @@
-print("Quizz: Quanto você sabe sobre o motorsport?")
-
+print("Quizz: Quanto você sabe sobre o motorsport")
+print("1- quando que a pista de interlagos fundada? \n A-2012 \n B-1938 \n C-1838")
+input("R: ").upper()
+if resposta == "B":
+    print("certa a resposta!")
+else:
+    print("errrrrouuu!")
 
 
 
@@ -9,3 +14,5 @@ print("Quizz: Quanto você sabe sobre o motorsport?")
 #--pergunta 2- qual é o piloto com mais campeonatos da stock car pro series/stock car brasil?
 #--resposta 2: ingo hoffman
 #--pergunta 3- quantos campeonatos de piloto o time Scuderia Ferrari ganhou na formula 1?
+
+##--Codigo Por Henrico William 2 F.D.S--
