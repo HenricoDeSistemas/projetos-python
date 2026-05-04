db7111a100a7c1563a7e50463db0a366721fe42d
HenricoDeSistemas <henricoiswilliam@gmail.com>
2026-05-04 15:48:17 +0000

    v1

---
+++ quizz.py
@@ -0,0 +1,11 @@
+print("Quizz: Quanto você sabe sobre o motorsport?")
+
+
+
+
+
+#--pergunta 1- quando que a pista de interlagos foi fundada?--
+#--resposta 1: 1938
+#--pergunta 2- qual é o piloto com mais campeonatos da stock car pro series/stock car brasil?
+#--resposta 2: ingo hoffman
+#--pergunta 3- quantos campeonatos de piloto o time Scuderia Ferrari ganhou na formula 1?
