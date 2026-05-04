5a6eaea692ba78188b22a859e9e2a61f8ca8cf7e
HenricoDeSistemas <henricoiswilliam@gmail.com>
2026-05-04 09:33:31 -0300

    v1

---
+++ seletor.py
@@ -0,0 +1,8 @@
+#--seletor de caracteres--
+letra = input("Digite a letra")
+if letra =="a":
+  print("voce escolheu a letra A)
+elif letra == "b":
+ print("voce escolheu a letra B")
+else:
+ print("essa opção não esta disponivel")
