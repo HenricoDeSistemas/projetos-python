859774dd89593583caadeffd7190306e5b835a59
HenricoDeSistemas <henricoiswilliam@gmail.com>
2026-05-25 12:50:52 +0000

    v1

---
+++ converter.py
@@ -0,0 +1,9 @@
+##ferramente de conversão dólar x real--
+def converter (valor_dolar):
+    taxa = 5.15
+    valor_real = valor_dolar * taxa
+    return valor_real
+print("conversor dolar x real")
+preco = float(input("digite o preço do produto em dolar:"))
+resultado = converter(preco)
+print(f"o valor em reais é:{resultado:.2f}")
