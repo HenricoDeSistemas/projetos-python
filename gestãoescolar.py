9358799698b92ff64aa49dcbc24b5d876bf6cead
HenricoDeSistemas <henricoiswilliam@gmail.com>
2026-05-25 12:32:14 +0000

    v1

---
+++ gestãoescolar.py
@@ -0,0 +1,10 @@
+### Definindo e chamando a função
+def exibir_cabecalho():
+    print('=' * 30)
+    print("sistema de gestão escolar")
+    print('versão 1.0 - desenvolvido no Codespaces')
+    print('=' * 30)
+
+### chamando a função
+exibir_cabecalho()
+print("menu principal:")
