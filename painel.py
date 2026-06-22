2e8b81ef024c4aeba8d37af2dff2496355a4bb0f
HenricoDeSistemas <henricoiswilliam@gmail.com>
2026-06-22 12:30:37 +0000

    v1

---
+++ painel.py
@@ -0,0 +1,6 @@
+print ("---PAINEL DE ATENDIMENTO---")
+fila_espera = ["Senha 01", "Senha 02", "Senha 03", "Senha 04,"]
+print(f"o segundo paciente é ser atendido é:{fila_espera [0]}")
+print(f"o segundo paciente é ser atendido é:{fila_espera [1]}")
+total = len(fila_espera)
+print(f"Total aguardando: {total}")
